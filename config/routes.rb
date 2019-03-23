67bee48a6e9f991fd4cc4d6b764ebe593aaead5b
Camilo Mason <camilomason@Camilos-MacBook-Pro.local>
2019-03-22 20:23:57 -0400

    nesting resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :albums
-  resources :songs
+  resources :albums do
+    resources :songs
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
